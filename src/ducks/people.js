e4ac9edac6653c42a4638472568c143c22a0599a
perovserg@mail.ru <perovserg@mail.ru>
2019-05-21 21:22:51 +0300

    add first saga with tests

--- src/ducks/people.js
+++ src/ducks/people.js
@@ -1,5 +1,8 @@
 import {Record, List} from 'immutable';
+import {put, call, takeEvery} from 'redux-saga/effects';
+
 import {appName} from '../config';
+import {generateId} from './utils';
 
 const ReducerState = Record({
     entities: new List([])
@@ -14,6 +17,7 @@ const PersonRecord = Record({
 
 export const moduleName = 'people';
 const prefix = `${appName}/${moduleName}`;
+export const ADD_PERSON_REQUEST = `${prefix}/ADD_PERSON_REQUEST`;
 export const ADD_PERSON = `${prefix}/ADD_PERSON`;
 
 
@@ -22,12 +26,34 @@ export default function reducer(state = new ReducerState(), action) {
 
     switch (type) {
         case ADD_PERSON:
-            return state.update("entities", entities => entities.push(new PersonRecord(payload.person)));
+            return state.update("entities", entities => entities.push(new PersonRecord(payload)));
         default:
             return state;
     }
 }
 
+export function addPerson(person) {
+    return {
+        type: ADD_PERSON_REQUEST,
+        payload: person
+    }
+}
+
+export const addPersonSaga = function * (action) {
+    const id = yield call(generateId); // "call(func, args)" => инструкция для вызова функции сагой
+    yield put({
+        type: ADD_PERSON,
+        payload: {...action.payload, id}
+    }); // вызовет dispatch с событием "ADD_PERSON"
+    // управление перейдет с reducer с type=ADD_PERSON
+};
+
+export const saga = function * () {
+    yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga);
+    // при событии "ADD_PERSON_REQUEST" выполнит сагу addPersonSaga
+};
+
+/*
 export function addPerson(person) {
     return (dispatch) => {
         dispatch({
@@ -38,3 +64,4 @@ export function addPerson(person) {
         });
     }
 }
+*/
--- src/redux/index.js
+++ src/redux/index.js
@@ -2,14 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'connected-react-router';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from './reducer';
 import history from '../history';
+import {saga} from '../ducks/people';
 
-const enhancer = applyMiddleware(routerMiddleware(history), thunk, logger);
+const sagaMiddleware = createSagaMiddleware();
+
+const enhancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger);
 
 const store = createStore(reducer, enhancer);
 
 window.store = store;
+
+sagaMiddleware.run(saga);
 
 export default store;
